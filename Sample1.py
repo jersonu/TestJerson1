7ee4d68571b3ec1efc6bbb1af4fdfffca9598739
Jerson <jersonu@gmail.com>
2014-10-14 11:26:43 -0300

    Create Sample1.py

---
+++ Sample1.py
@@ -0,0 +1,13 @@
+def CelsiusToF():
+    C = input ("Enter degrees in C ->")
+    F = (9.0 /5.0) * C +32
+    print C, "Celsius = ", F, "Fahrenheit"
+    
+
+def Loopy():
+    for a in range(0,10,2):
+        print a
+        
+def Dic(i):
+    allWords = ['abc', 'def', 'ghi','jkl']
+    print allWords[i]
